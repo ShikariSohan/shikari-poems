4f65dbe24581e92245ae050566d430e0a268628a
Moksedur Rahman Sohan <shikari@Moksedurs-MacBook-Pro.local>
2025-02-20 22:59:46 +0600

    Added new poem page 2

--- src/app/pages/page2/page2.component.ts
+++ src/app/pages/page2/page2.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-page2',
   templateUrl: './page2.component.html',
+  styleUrls: ['./page2.component.scss'],
   standalone: true,
-  imports: [CommonModule]
+  imports: [CommonModule],
 })
-export class Page2Component {
-
-}
+export class Page2Component {}
